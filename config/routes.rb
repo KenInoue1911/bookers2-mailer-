c91d8694fcf5c7e003aa0194c22454984af26913
KenInoue1911 <inouetkrk3@gmail.com>
2021-05-06 06:22:20 +0000

    5/6

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
 
   root :to => "homes#top"
   get "home/about" => "homes#about"
+  get "/search", to: "searchs#search"
 
-  
+
   post "follow/:id" => "relationships#follow", as: "follow"
   post "unfollow/:id" => "relationships#unfollow", as: "unfollow"
-  
+
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
-    member do 
+    member do
     get :following, :follower
     end
- end    
+ end
 end
